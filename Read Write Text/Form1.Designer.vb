0c6357e2161510002366ca0732606560a22fa720
JujuJuhu <jovita.khu@gmail.com>
2024-02-07 15:07:42 +0700

    Add project files.

---
+++ Read Write Text/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(228, 34)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(365, 46)
+        Label1.TabIndex = 0
+        Label1.Text = "Read Write Text Form"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        Button1.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = SystemColors.ButtonFace
+        Button1.Location = New Point(46, 111)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(160, 79)
+        Button1.TabIndex = 1
+        Button1.Text = "Create .txt File"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        Button2.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.ForeColor = SystemColors.ButtonFace
+        Button2.Location = New Point(46, 210)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(160, 79)
+        Button2.TabIndex = 2
+        Button2.Text = "Write to File"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        Button3.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.ForeColor = SystemColors.ButtonFace
+        Button3.Location = New Point(46, 311)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(160, 79)
+        Button3.TabIndex = 3
+        Button3.Text = "Read from File"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(228, 111)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(508, 279)
+        TextBox1.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(0), CByte(0), CByte(64))
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        ForeColor = Color.White
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ Read Write Text/Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim newfile As String = "C:\TEXT FILE\MyFile.txt"
+        Dim fs As FileStream = File.Create(newfile)
+        MessageBox.Show("Successfully Created File.")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim text As New StringBuilder
+        text.AppendLine(TextBox1.Text)
+        Dim filepath As String = "C:\TEXT FILE\MyFile.txt"
+        File.AppendAllText(filepath, text.ToString())
+        MessageBox.Show("Success.")
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim gettext As String = My.Computer.FileSystem.ReadAllText("C:\TEXT FILE\MyFile.txt")
+        TextBox1.Text = gettext
+    End Sub
+End Class
